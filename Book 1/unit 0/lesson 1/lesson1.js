6aed4b183cc310c16ea1162278872b793c2d5f99
DecamLekgau <161304335+DecamLekgau@users.noreply.github.com>
2024-10-08 20:37:08 +0200

    Add files via upload

---
+++ Book 1/unit 0/lesson 1/lesson1.js
@@ -0,0 +1,12 @@
+3 + 3
+3 / 0
+console.log("Heloo javascript");
+let name = "Thabo Lekgau";
+console.log(name);
+class Goat {
+eat(foodType) {
+console.log(`I love eatin ${foodType}`);
+}
+}
+let billy = new Goat();
+billy.eat("tin cans");
